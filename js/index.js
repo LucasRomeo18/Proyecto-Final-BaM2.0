44308ad07b153dc850054ad1e89d7b1e9b46c979
LucasRomeo18 <lucas.romeo18@gmail.com>
2023-05-07 20:04:58 -0300

    web

---
+++ js/index.js
@@ -0,0 +1,24 @@
+// comentario de linea control mas corchete de cierre
+
+// let variable2; //Declarada
+
+// const LENGUAJE = "JAVASCRIPT"; //inicializada
+
+// variable2 = 2;
+
+// let variable3 = "un texto";
+
+
+// console.log(variable3)
+
+
+let nombre = "Lucas";
+let apellido = "Romeo";
+let edad = 33;
+let tieneMascota = "Si tiene";
+
+
+console.log("El nombre es "+nombre);
+console.log("El apellido es "+ apellido);
+console.log("La edad es "+ edad);
+console.log("Tiene mascota? " + tieneMascota);
