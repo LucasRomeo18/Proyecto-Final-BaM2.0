98a070019ffa8edc3218c36f386bb079e2cd3f60
LucasRomeo18 <lucas.romeo18@gmail.com>
2023-06-05 23:49:59 -0300

    arreglado la pagina de habilidades, falta boton

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,32 +1,19 @@
 
-window.onload = function() {
-  var botones = document.getElementsByClassName("miBoton");
+// Funcion para ocultar o mostrar el porcentaje en la pagina de habilidades
 
-  for (var i = 0; i < botones.length; i++) {
-    var boton = botones[i];
-    var porcentaje = 0; // Porcentaje inicial
-    var porcentajeCirculo = boton.querySelector(".porcentaje-circulo");
-    var porcentajeBorde = boton.querySelector(".porcentaje-borde");
-    var porcentajeNumero = boton.querySelector(".porcentaje-numero");
-
-    boton.addEventListener("click", function() {
-      if (porcentaje === 0) {
-        porcentaje = 80; // Porcentaje a mostrar
-        porcentajeCirculo.style.display = "block";
-        porcentajeNumero.style.display = "block";
-      } else {
-        porcentaje = 0;
-        porcentajeCirculo.style.display = "none";
-        porcentajeNumero.style.display = "none";
-      }
-
-      // Establecer el tamaño del borde circular según el porcentaje
-      var circunferencia = 2 * Math.PI * porcentajeBorde.getAttribute("r");
-      var longitudDasharray = (100 - porcentaje) / 100 * circunferencia;
-      porcentajeBorde.style.strokeDasharray = longitudDasharray + " " + circunferencia;
-
-      // Actualizar el texto del porcentaje
-      porcentajeNumero.textContent = porcentaje + "%";
-    });
+function togglePorcentaje() {
+    var porcentaje = document.querySelector('.porcentaje');
+    porcentaje.style.display = (porcentaje.style.display === 'none') ? 'block' : 'none';
   }
-};
+
+
+//   Confirmacion de envio de formulario
+
+function confirmacion() {
+    var respuesta = confirm("¿Estás seguro de enviar el formulario?");
+    if (respuesta == true) {
+      alert("Formulario enviado correctamente.");
+    } else {
+      alert("El envío del formulario ha sido cancelado.");
+    }
+  }
