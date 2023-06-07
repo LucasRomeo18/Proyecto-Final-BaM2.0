dfb7c7dc654015d795b8c0ddee87b1ff130aeaad
Lucas <lucas.romeo18@gmail.com>
2023-06-07 12:45:28 -0300

    arreglado en la facu

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,12 +1,4 @@
 
-// Funcion para ocultar o mostrar el porcentaje en la pagina de habilidades
-
-function togglePorcentaje() {
-    var porcentaje = document.querySelector('.porcentaje');
-    porcentaje.style.display = (porcentaje.style.display === 'none') ? 'block' : 'none';
-  }
-
-
 //   Confirmacion de envio de formulario
 
 function confirmacion() {
@@ -16,46 +8,69 @@ function confirmacion() {
     } else {
       alert("El envío del formulario ha sido cancelado.");
     }
-  }
+}
 
-  // Funciones para mostrar con una alerta el conocimiento que poseo en cada programa
+// Funciones para mostrar con una alerta el conocimiento que poseo en cada programa
 
-  function psAlerta(){
+function psAlerta(){
     alert("Mi conocimiento de Photoshop es de 80%")
-  }
+}
 
-  function ilAlerta(){
-    alert("Mi conocimiento de Photoshop es de 80%")
-  }
+function ilAlerta(){
+    alert("Mi conocimiento de Illustrator es de 80%")
+}
 
-  function aeAlerta(){
-    alert("Mi conocimiento de Photoshop es de 80%")
-  }
+function aeAlerta(){
+    alert("Mi conocimiento de After Effects es de 60%")
+}
 
-  function idAlerta(){
-    alert("Mi conocimiento de Photoshop es de 80%")
-  }
+function idAlerta(){
+    alert("Mi conocimiento de Indesign es de 30%")
+}
 
-  function xdAlerta(){
-    alert("Mi conocimiento de Photoshop es de 80%")
-  }
+function xdAlerta(){
+    alert("Mi conocimiento de Adobe xD es de 50%")
+}
 
-  function prAlerta(){
-    alert("Mi conocimiento de Photoshop es de 80%")
-  }
+function prAlerta(){
+    alert("Mi conocimiento de Premier es de 20%")
+}
 
-  function figmaAlerta(){
-    alert("Mi conocimiento de Photoshop es de 80%")
-  }
+function figmaAlerta(){
+    alert("Mi conocimiento de Figma es de 70%")
+}
 
-  function htmlAlerta(){
-    alert("Mi conocimiento de Photoshop es de 80%")
-  }
+function htmlAlerta(){
+    alert("Mi conocimiento de HTML es de 40%")
+}
 
-  function cssAlerta(){
-    alert("Mi conocimiento de Photoshop es de 80%")
-  }
+function cssAlerta(){
+    alert("Mi conocimiento de CSS es de 40%")
+}
 
-  function jsAlerta(){
-    alert("Mi conocimiento de Photoshop es de 80%")
+function jsAlerta(){
+    alert("Mi conocimiento de Javascript es de 30%")
+}
+
+
+// Menu Hamburgesa
+
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger= document.querySelector(".hamburger");
+const closeIcon= document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
   }
+}
+
+hamburger.addEventListener("click", toggleMenu);
