c55e94a8f9d9ec34b9ccdf0ff325b7e714ffb05d
Lucas <lucas.romeo18@gmail.com>
2023-06-07 07:57:07 -0300

    facu

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,32 +1,15 @@
+// Menu hamburguesa
 
-window.onload = function() {
-  var botones = document.getElementsByClassName("miBoton");
 
-  for (var i = 0; i < botones.length; i++) {
-    var boton = botones[i];
-    var porcentaje = 0; // Porcentaje inicial
-    var porcentajeCirculo = boton.querySelector(".porcentaje-circulo");
-    var porcentajeBorde = boton.querySelector(".porcentaje-borde");
-    var porcentajeNumero = boton.querySelector(".porcentaje-numero");
+// Funcion par mostra los porcentajes para la pagina de habilidades
 
-    boton.addEventListener("click", function() {
-      if (porcentaje === 0) {
-        porcentaje = 80; // Porcentaje a mostrar
-        porcentajeCirculo.style.display = "block";
-        porcentajeNumero.style.display = "block";
-      } else {
-        porcentaje = 0;
-        porcentajeCirculo.style.display = "none";
-        porcentajeNumero.style.display = "none";
-      }
+// function togglePorcentaje(button) {
+//   var porcentaje = button.querySelector(".porcentaje");
 
-      // Establecer el tamaño del borde circular según el porcentaje
-      var circunferencia = 2 * Math.PI * porcentajeBorde.getAttribute("r");
-      var longitudDasharray = (100 - porcentaje) / 100 * circunferencia;
-      porcentajeBorde.style.strokeDasharray = longitudDasharray + " " + circunferencia;
+//   if (porcentaje.style.display === "none") {
+//       porcentaje.style.display = "block";
+//   } else {
+//       porcentaje.style.display = "none";
+//   }
+// }
 
-      // Actualizar el texto del porcentaje
-      porcentajeNumero.textContent = porcentaje + "%";
-    });
-  }
-};
