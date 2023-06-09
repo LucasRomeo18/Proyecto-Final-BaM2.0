a292d0f430260b7759f6cc4281d8ed0ec6bd94cc
LucasRomeo18 <lucas.romeo18@gmail.com>
2023-06-09 00:21:53 -0300

    arreglado el menu

--- Javascript/index.js
+++ Javascript/index.js
@@ -55,15 +55,18 @@ function jsAlerta(){
 
 // Menu Hamburgesa
 
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menu");
+  
+    hamburger.addEventListener("click", () => {
+      hamburger.classList.toggle("active");
+      navMenu.classList.toggle("active");
+    });
+  
+    document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+      hamburger.classList.remove("active");
+      navMenu.classList.remove("active");
+    }));
+  });
 
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
-
-document-querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",() =>{
-   hamburger.classList.remove("active");
-   navMenu.classList.remove("active");
- }));
